d81d8517c93a0356cc387287483a54dbc5d170d0
Yulia Kordunova <kordunovayulia@gmail.com>
2021-08-29 19:37:35 +0300

    added third result

--- src/Third.java
+++ src/Third.java
@@ -1,2 +1,7 @@
 public class Third {
+    // Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        double volume = length*width*height;
+        return volume;
+    }
 }
