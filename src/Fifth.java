e4966fbcd0e7a9fbb6a4cfc9da1eca4ea21736aa
Yulia Kordunova <kordunovayulia@gmail.com>
2021-08-30 16:05:39 +0300

    added fifth file

---
+++ src/Fifth.java
@@ -0,0 +1,14 @@
+public class Fifth {
+    public static String greet(String name) {
+
+
+        if(name.equals("Johnny")){
+            return "Hello, my love!";
+        }
+
+        else
+            return String.format("Hello, %s!", name);
+
+
+    }
+}
